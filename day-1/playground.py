4a29430ee80225e971795fa37f9a2894287cf03b
hjongh <hrjang0916@gmail.com>
2020-11-13 18:31:51 -0800

    Add some examples to day-1 playground

--- day-1/playground.py
+++ day-1/playground.py
@@ -1,2 +1,35 @@
 # This is a program
 print('Hello World')
+
+# You can print any string
+print('Hello Bob')
+
+# Numbers and variables
+x = 4
+banana = 7
+y = x + banana
+print(y)
+z = x * banana + y
+print(z)
+
+# Strings
+a = 'abc'
+
+# Basic string operations
+b = 'cde'
+c = a + b
+d = 4 * a
+
+# Indexing strings
+e = a[0]
+f = a[len(a) - 1]
+g = a[-1]
+
+# Substrings
+h = a[1:len(a) - 1]
+print(e)
+
+# Lists
+numbers = [1, 2, 3]
+numbersLen = len(numbers)
+print(numbersLen)
