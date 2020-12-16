0c66952f42b98fd73e51c10bce0c8ef5afbaeb49
hjongh <hrjang0916@gmail.com>
2020-12-15 17:35:03 -0800

    minor fixes

--- project-1/5-exercise-input-validation.py
+++ project-1/5-exercise-input-validation.py
@@ -6,9 +6,17 @@ Write a function starts_with_123():
 """
 
 
+# Test your code
+starts_with_123()
+
+
 """
 Write a function starts_with():
     Parameters: a string
     Side Effect: Asks user for user input
     Returns: True if the input starts with the given string, False otherwise
 """
+
+
+# Test your code
+starts_with('cat')
