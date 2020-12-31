5961d3b8cba17fc0eb66b9166df4035e1d457d5e
hjongh <hrjang0916@gmail.com>
2020-12-31 14:34:12 -0800

    add another 2d list exercise

---
+++ week-5/exercises/2-5-2d-lists.py
@@ -0,0 +1,15 @@
+import util.test as test
+
+
+"""
+Define a function len_longest_row():
+    Parameters: a 2D list
+    Returns: The length of the longest row in the 2D list
+"""
+# Start of your code
+
+# End of your code
+
+
+test.test_equals(len_longest_row([["a"], ["c", "d"]]), 2)
+test.test_equals(len_longest_row([["a", "b", 3], [1, 2]]), 3)
