032a64587a6a02d219de66c5a91cbff33d1c75b3
hjongh <hrjang0916@gmail.com>
2021-01-21 23:19:22 -0800

    add project 5

--- project-5/3-playground.py
+++ project-5/3-playground.py
@@ -1,1 +1,14 @@
-# Splitting with multiple separators
+# Reading Files
+file = open('txt-files/5-read-file.txt', 'r')
+print(file.read())
+file.close()
+
+# Appending to Files
+file = open('txt-files/5-append-file.txt', 'a')
+file.write('This is extra file content')
+file.close()
+
+# Writing to Files
+file = open('txt-files/5-write-file.txt', 'w')
+file.write('Overwriting file content')
+file.close()
---
+++ project-5/4-project.py
@@ -0,0 +1,31 @@
+"""
+Define a function play_madlibs():
+    Parameters: N/A
+    Side Effect: Plays a game of madlibs.
+        Steps:
+            1. Ask user for Madlibs filename
+            2. Get the content of the file and split it into words
+            3. Construct a completed Madlib by looping over each word, and
+                asking the user for replacement on a "Madlib word".
+            4. Print out completed Madlib
+    Returns: N/A
+"""
+
+
+"""
+Define a function get_filename():
+    Parameters: N/A
+    Side Effect: asks user for Madlibs filename
+    Returns: Madlibs filename (string)
+"""
+
+
+"""
+Define a function get_madlib_word_replacement():
+    Parameters: original Madlib word
+    Side Effect: asks user for Madlib word replacement
+    Returns: Madlibs word replacement (string)
+"""
+
+
+play_madlibs()
