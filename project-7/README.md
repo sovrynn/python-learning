ff245d1c23037a4bbebb491fe7c13026a25f5731
HJ <fay@hjs-macbook-pro.attlocal.net>
2021-07-03 12:27:56 -0700

    add project 7 material

---
+++ project-7/README.md
@@ -0,0 +1,13 @@
+# Object-oriented Pokemon game
+
+Class 1: Pokemon
+- HP
+- Name
+- Nickname
+- Moves
+- Type
+
+Class 2: Move
+- Name
+- Damage
+- Type
---
+++ project-7/project.py
@@ -0,0 +1,11 @@
+# Step 1: Define Classes
+
+# Step 2: Create Pokemon and Move objects
+
+# Step 3: Implement the game
+
+def main():
+    print("TODO")
+
+
+main()
